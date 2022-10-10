ee1b1bdaae705f75b8a1655a1495db8d41ccade3
Muhammad Asim <muhammadasim4515@gmail.com>
2022-10-10 15:29:01 +0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+import HomePage from "../screens/HomePage";
+
+export default function Home() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
---
+++ styles/global.css.js
@@ -0,0 +1,8 @@
+export const color = {
+  primaryColor: "#FFAA4D",
+  secondaryColor: "#6762F0",
+  lightColor: "#FFFFFF",
+  accentColor: "#AEAEAE",
+  darkColor: "#000000",
+  lightShade: "#FFF6E2",
+};
