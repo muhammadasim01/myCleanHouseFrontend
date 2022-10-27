a7e026455b7c4335a696f36600c283664ff52b36
azhar736 <azhark3113@gmail.com>
2022-10-27 14:31:51 +0500

    Adding Toggle Button to Mobile Menu

--- components/MobileHeader/SignInSignUp.js
+++ components/MobileHeader/SignInSignUp.js
@@ -4,30 +4,43 @@ import SignUp from "./SignUp";
 
 function SignInSignUp() {
   const [isSignIn, setIsSignIn] = useState(true);
-  const [isSignUp, setIsSignUp] = useState(false);
   console.log("The State1 is", isSignIn);
-  console.log("The State2 is", isSignUp);
+  const handleSignInButton = () => {
+    if (!isSignIn) {
+      setIsSignIn(true);
+    }
+  };
+  const handleSignUpButton = () => {
+    if (isSignIn) {
+      setIsSignIn(false);
+    }
+  };
   return (
-    <div className="flex flex-col h-auto border-2 justify-between items-center">
+    <div className="flex flex-col h-auto justify-between items-center">
       <h2 className="font-heading text-3xl font-bold py-8 px-5">
         Sign In or Sign Up
       </h2>
-      <div className="border-2 w-48 flex justify-between px-2 py-1 mb-5 bg-accentColor rounded-full">
+      <div className="w-48 flex justify-between  mb-5 bg-accentColor rounded-full">
         <button
-          onClick={() => setIsSignIn(!false)}
-          className="font-text text-lightColor text-base font-semibold"
+          // onClick={() => setIsSignIn(!false)}
+          className={`font-text text-lightColor text-base font-semibold w-[50%] py-1 rounded-full ${
+            isSignIn && "bg-darkColor"
+          }`}
+          onClick={handleSignInButton}
         >
           Sign In
         </button>
         <button
-          onClick={() => setIsSignUp(!false) && setIsSignIn(false)}
-          className="font-text text-lightColor text-base font-semibold"
+          // onClick={() => setIsSignUp(!false) && setIsSignIn(false)}
+          className={`font-text text-lightColor text-base font-semibold w-[50%] py-1 rounded-full ${
+            !isSignIn && "bg-darkColor"
+          }`}
+          onClick={handleSignUpButton}
         >
           Sign Up
         </button>
       </div>
-      <SignIn />
-      {/* <SignUp /> */}
+      {isSignIn ? <SignIn /> : <SignUp />}
     </div>
   );
 }
